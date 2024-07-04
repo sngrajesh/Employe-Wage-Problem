024b8e0d62bbe7a320181f1ef3bb30ad6cfb4f89
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-04 18:06:51 +0530

    [Rajesh] Modified : Employee_Wage_Problem

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random
 
+WAGE_PER_HOUR = 20
+FULL_DAY_HOUR = 8
+
 def check_attendance(name):
     # Generate a random attendance value between 0 and 1
     attendance = random.uniform(0, 1) > 0.5
@@ -7,10 +10,18 @@ def check_attendance(name):
         print(f'Employee {name} is present')
     else:
         print(f'Employee {name} is absent')
+    return attendance
+
+def calculate_daily_wage(attendance):
+    if attendance:
+        return WAGE_PER_HOUR*FULL_DAY_HOUR
+    else:
+        return 0
+
 
 name = input('Enter your name: ')
 if len(name) > 3 and name.isalpha():
-    check_attendance(name)
+    attendance = check_attendance(name)
+    print(f'{name} wage is {calculate_daily_wage(attendance)}')
 else:
     print('Enter a valid name')
-
