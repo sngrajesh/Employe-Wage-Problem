617fafed834202822b0ac66c8552a3c1b3e04428
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-04 18:27:56 +0530

    [Rajesh] Modified : Employee_Parttime_Wage_Problem

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ import random
 
 WAGE_PER_HOUR = 20
 FULL_DAY_HOUR = 8
+PART_TIME_HOUR = 4
 
 def check_attendance(name):
     # Generate a random attendance value between 0 and 1
-    attendance = random.uniform(0, 1) > 0.5
-    if attendance:
+    attendance = random.randint(0, 2)
+    if attendance == 0:
         print(f'Employee {name} is present')
+    elif attendance == 1:
+        print(f'Employee {name} is on Part-time')
     else:
         print(f'Employee {name} is absent')
+    
     return attendance
 
 def calculate_daily_wage(attendance):
-    if attendance:
+    if attendance == 0:
         return WAGE_PER_HOUR*FULL_DAY_HOUR
+    elif attendance == 1:
+        return WAGE_PER_HOUR*PART_TIME_HOUR
     else:
         return 0
 
