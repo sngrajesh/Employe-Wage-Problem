d675e5ff6f3b0a329567eaf5eac861227a0b8d42
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-05 12:46:58 +0530

    [Rajesh] Add : Monthly Wage Problem

--- main.py
+++ main.py
@@ -4,29 +4,22 @@ WAGE_PER_HOUR = 20
 FULL_DAY_HOUR = 8
 PART_TIME_HOUR = 4
 
-def check_attendance(name):
+def check_attendance():
     """
-    Descrption : Generate Random number and check Employee is present or not 
-    Input : {name} of the employee
+    Description : Generate Random number and check Employee is present or not 
+    Input : None
     Output : {attendance} of 1 day
     """
-
-    # Generate a random attendance value between 0 and 1
     attendance = random.randint(0, 2)
-    match attendance:
-        case 0:
-            print(f'Employee {name} is present')
-        case 1:
-            print(f'Employee {name} is on Part-time')
-        case _:
-            print(f'Employee {name} is absent')
     return attendance
+
+
 
 def calculate_daily_wage(attendance):
     """
-    Descrption : Calcualate daily wage of employee based on their atendance on attendance
+    Description : Calcualate daily wage of employee based on their atendance on attendance
     Input : {attendance} of one day
-    Output : {wag} calculated 
+    Output : {waga} calculated 
     """
     match attendance:
         case 0:
@@ -34,12 +27,28 @@ def calculate_daily_wage(attendance):
         case 1:
             return WAGE_PER_HOUR*PART_TIME_HOUR
         case _:
-            return 0
+            return 0  
+
+
+
+def monthly_wage_of_employee ():
+    """
+    Description : Calcualate monthly wage of employee based on their atendance on attendance
+    Input : None 
+    Output : {monthly_wage} of month
+    """
+
+    monthly_wage = 0
+    for i in range(0,20):
+        monthly_wage += calculate_daily_wage(check_attendance())
+    
+    return monthly_wage
+    
 
 
 name = input('Enter your name: ')
 if len(name) > 3 and name.isalpha():
-    attendance = check_attendance(name)
-    print(f'{name} wage is {calculate_daily_wage(attendance)}')
+    monthly_wage = monthly_wage_of_employee()
+    print(f'{name} wage is {monthly_wage}')
 else:
     print('Enter a valid name')
