b085b1c2f366ea7fa69fcc6952cf8a3f5df4ef0d
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-05 15:17:14 +0530

    [Rajesh] Add : Monthly Wage Problem Using Class

--- main.py
+++ main.py
@@ -1,60 +1,57 @@
 import random
 
-WAGE_PER_HOUR = 20
-FULL_DAY_HOUR = 8
-PART_TIME_HOUR = 4
-
-def check_attendance():
-    """
-    Description : Generate Random number and check Employee is present or not 
-    Input : None
-    Output : {attendance} of 1 day
-    """
-    attendance = random.randint(0, 2)
-    return attendance
-
-
-
-def calculate_daily_wage(attendance):
-    """
-    Description : Calcualate daily wage of employee based on their atendance on attendance
-    Input : {attendance} of one day
-    Output : {wage, hour} object calculated 
-    """
-    match attendance:
-        case 0:
-            return {'wage' : WAGE_PER_HOUR*FULL_DAY_HOUR, 'hour' : 8}
-        case 1:
-            return {'wage' : WAGE_PER_HOUR*PART_TIME_HOUR, 'hour' : 4}
-        case _:
-            return {'wage' : 0, 'hour' : 0}
-
-
-
-def monthly_wage_of_employee ():
-    """
-    Description : Calcualate monthly wage of employee based on their atendance on attendance and limit of working hour
-    Input : None 
-    Output : {monthly_wage} of month
-    """
-
-    monthly_wage = 0
-    total_hour = 0
+class EmployeeAttendance:
+    WAGE_PER_HOUR = 20
+    FULL_DAY_HOUR = 8
+    PART_TIME_HOUR = 4
     
-    for i in range(0,20):
-        data = calculate_daily_wage(check_attendance())
-        monthly_wage += data['wage']
-        total_hour += data['hour']
-        if(total_hour >= 100):
-            break
-
-    return {'monthly_wage' : monthly_wage,'total_hour':total_hour}
+    @staticmethod
+    def check_attendance():
+        """
+        Description : Generate Random number and check Employee is present or not 
+        Input : None
+        Output : {attendance} of 1 day
+        """
+        attendance = random.randint(0, 2)
+        return attendance
     
+    @classmethod
+    def calculate_daily_wage(cls, attendance):
+        """
+        Description : Calculate daily wage of employee based on their attendance
+        Input : {attendance} of one day
+        Output : {wage, hour} object calculated 
+        """
+        match attendance:
+            case 0:
+                return {'wage': cls.WAGE_PER_HOUR * cls.FULL_DAY_HOUR, 'hour': 8}
+            case 1:
+                return {'wage': cls.WAGE_PER_HOUR * cls.PART_TIME_HOUR, 'hour': 4}
+            case _:
+                return {'wage': 0, 'hour': 0}
+    
+    @classmethod
+    def monthly_wage_of_employee(cls):
+        """
+        Description : Calculate monthly wage of employee based on their attendance and limit of working hour
+        Input : None 
+        Output : {monthly_wage} of month
+        """
+        monthly_wage = 0
+        total_hour = 0
+        
+        for i in range(0, 20):
+            data = cls.calculate_daily_wage(cls.check_attendance())
+            monthly_wage += data['wage']
+            total_hour += data['hour']
+            if total_hour >= 100:
+                break
+        
+        return {'monthly_wage': monthly_wage, 'total_hour': total_hour}
 
 
-name = input('Enter your name: ')
-if len(name) > 3 and name.isalpha():
-    data = monthly_wage_of_employee()
-    print(f"{name} wage is {data['monthly_wage']} for {data['total_hour']}")
-else:
-    print('Enter a valid name')
+
+if __name__ == "__main__":
+    employee = EmployeeAttendance()
+    data = employee.monthly_wage_of_employee()
+    print(f"Wage is {data['monthly_wage']} for {data['total_hour']}")
